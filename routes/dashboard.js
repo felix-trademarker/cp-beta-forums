f1695e2ab52a270ce2525dd60499dfab4f311072
Felix <felix.bongo@tweeklabs.com>
2022-04-05 11:56:40 +0800

    update

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -9,19 +9,19 @@ var router = express.Router();
 // TESTERS ============================
 // ====================================
 
-router.get(['/', '/beta','/beta-app'],dashboardController.landing);
-router.get(['/forums', '/beta/forums'],dashboardController.forum);
-router.get(['/forums/:id','/beta/forums/:id'],dashboardController.forumPage);
+router.get('/',dashboardController.landing);
+router.get('/forums',dashboardController.forum);
+router.get('/forums/:id',dashboardController.forumPage);
 
-router.get(['/download','/beta/download'],dashboardController.downloadPage);
-router.get(['/we-need-you','/beta/we-need-you'],dashboardController.weNeedYou);
-router.get(['/our-mission','/beta/our-mission'],dashboardController.ourMission);
-router.get(['/become-a-beta-tester','/beta/become-a-beta-tester'],dashboardController.enroll);
-router.get(['/about','/beta/about'],dashboardController.about);
+router.get('/download',dashboardController.downloadPage);
+router.get('/we-need-you',dashboardController.weNeedYou);
+router.get('/our-mission',dashboardController.ourMission);
+router.get('/become-a-beta-tester',dashboardController.enroll);
+router.get('/about',dashboardController.about);
 // router.get('/dashboard-topics/:topics',dashboardController.topics);
 
-router.post(['/forums', '/beta/forums'],dashboardController.addSubTopicsClient);
-router.post(['/forums/participate', '/beta/forums/participate'],dashboardController.participate);
+router.post('/forums',dashboardController.addSubTopicsClient);
+router.post('/forums/participate',dashboardController.participate);
 
 
 // ADMIN ==============================
