b12a1b9cef639a1a9e405964526080de44b7767f
Felix <felix.bongo@tweeklabs.com>
2022-04-25 18:03:34 +0800

    update

--- public/beta/lesson/script.js
+++ public/beta/lesson/script.js
@@ -153,3 +153,20 @@ if (Notification.permission === 'granted') {
         }
     })
 }
+
+
+
+var vuex = JSON.parse(window.localStorage.getItem('vuex'))
+
+fetch('api/v1/lessons/get-lesson?slug=hello-and-goodbye',{
+    credentials: 'same-origin',
+    headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '+ vuex.token
+    },
+})
+.then(resp => resp.json())
+.then(res => {
+    lessonDashboardApp.lesson = res
+})
